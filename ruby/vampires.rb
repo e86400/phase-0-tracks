909183ebf65e779eaa008c651f9f755b6932b7b7
Elizabeth Martinez <elizabethelzymartinez@gmail.com>
2017-02-15 21:07:43 -0600

    Add coding content to vampires.rb for 4.4 hw

---
+++ ruby/vampires.rb
@@ -0,0 +1,90 @@
+
+puts "How many employees will be processed?"
+employees = gets.to_i
+x = 0
+	until x == employees
+
+puts "What is your name?"
+name = gets.chomp
+	case name
+		when "Drake Cula", "Tu Fang"
+			weird_name = true
+		else
+			weird_name = false
+		end
+
+puts "How old are you?"
+age = gets.to_i
+
+puts "What year were you born?"
+born = gets.to_i
+	if 2017-born == age
+		year_born = true
+	else
+		year_born = false
+	end
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+garlic = gets.chomp
+
+	if garlic == "y"
+		eats_garlic = true
+	else
+		eats_garlic = false
+	end
+
+puts "Would you like to enroll in our company's health insurance plan? (y/n)"
+insurance = gets.chomp
+
+	if insurance ="y"
+		wants_insurance = true
+	else 
+		wants_insurance = false
+	end
+
+
+
+# finish this part --------------------------
+# allergies = gets.chomp
+#	until "done" || "sunshine"
+#		break
+#	if allergies = "sunshine"
+#		puts "Probably a vampire."
+#	end
+
+prompt = "> "
+puts "Please list allergies one at a time, then type done when finished."
+print prompt
+
+#while user_input = gets.chomp # loop while getting user input
+#  case user_input
+#  when "sunshine"
+#   puts "Probably a vampire."
+#    break 
+#  when gets.chomp
+#    puts "Second response"
+#    break 
+# else
+#   puts "Invalid user input. Please try again."
+#   print prompt 
+# end
+#end 
+#
+	if year_born && (eats_garlic || wants_insurance)
+		puts "Probably not a vampire."
+	elsif !year_born && (!eats_garlic || !wants_insurance)
+		puts "Probably a vampire."
+	elsif !year_born && !eats_garlic && !wants_insurance
+		puts "Almost certainly a vampire."
+	elsif weird_name 
+		puts "Definitely a vampire."
+	else
+		puts "Results inconclusive."
+	end
+
+
+x += 1
+
+
+ "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
+end
