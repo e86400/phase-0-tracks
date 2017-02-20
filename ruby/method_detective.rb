8484eabcc4e42b8f4c6377196111060cf5f9c329
Elizabeth Martinez <elizabethelzymartinez@gmail.com>
2017-02-20 16:56:30 -0600

    Add method call codes to 5.1 for method_detective.rb

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,38 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+ "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+ "zom".gsub('o', 'oo')
 # => “zoom”
 
-# "enhance".<???>
+ "enhance".center(16)
+# OR "enhance.center( "enhance".length + 8 )
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+ "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+ "the usual".insert 9, " suspects"
 #=> "the usual suspects"
 
-# " suspects".<???>
+ " suspects".insert 0, "the usual"
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+ "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+ "The mystery of the missing first letter".sub('T', '')
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+ "Elementary,    my   dear        Watson!".squeeze(" ")
+# OR "Elementary,    my   dear        Watson!".split.join(" ")
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+ "z".codepoints
 # => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+# (What is the significance of the number 122 in relation to the character z?) It is the ASCII code. When you type and hold alt+122, you get z 
 
-# "How many times does the letter 'a' appear in this string?".<???>
+ "How many times does the letter 'a' appear in this string?".count"a"
 # => 4
