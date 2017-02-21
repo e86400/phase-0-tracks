380f34afa8295bf15acf0a317fb496bec4650da1
Elizabeth Martinez <elizabethelzymartinez@gmail.com>
2017-02-20 22:43:53 -0600

    Add puts and changes to 5.1

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,38 +4,37 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
- "iNvEsTiGaTiOn".swapcase
+puts "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
- "zom".gsub('o', 'oo')
+puts "zom".gsub('o', 'oo')
 # => “zoom”
 
- "enhance".center(16)
-# OR "enhance.center( "enhance".length + 8 )
+puts "enhance".center(16)
 # => "    enhance    "
 
- "Stop! You’re under arrest!".upcase
+puts "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
- "the usual".insert 9, " suspects"
-#=> "the usual suspects"
-
- " suspects".insert 0, "the usual"
+puts "the usual".insert 9, " suspects"
 # => "the usual suspects"
 
- "The case of the disappearing last letter".chop
+puts " suspects".insert 0, "the usual"
+# => "the usual suspects"
+
+puts "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
- "The mystery of the missing first letter".sub('T', '')
+puts "The mystery of the missing first letter".sub('T', '')
 # => "he mystery of the missing first letter"
 
- "Elementary,    my   dear        Watson!".squeeze(" ")
-# OR "Elementary,    my   dear        Watson!".split.join(" ")
+puts "Elementary,    my   dear        Watson!".squeeze(" ")
+puts "Elementary,    my   dear        Watson!".split.join(" ")
 # => "Elementary, my dear Watson!"
 
- "z".codepoints
+puts "z".sum
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?) It is the ASCII code. When you type and hold alt+122, you get z 
 
- "How many times does the letter 'a' appear in this string?".count"a"
+puts "How many times does the letter 'a' appear in this string?".count"a"
 # => 4
