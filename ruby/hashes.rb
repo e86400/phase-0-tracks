cf84492ea07402c791a7aceb4a571455efbb2401
Elizabeth Martinez <elizabethelzymartinez@gmail.com>
2017-02-21 15:31:46 -0600

    Add minor changes to wip hashes.rb for 5.2

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -3,15 +3,60 @@
 # Ask the user to input informatiom
 # Create 
 
+h = {} 
+#h["a"] = "apple"
+#h["a"] = "dog"
+puts "enter age"
+age = gets.chomp
+p age
+#p h 
+
+
+#input = ""
+
+#until input == "Done"
+	# your code here
+#end
 
 
 puts "Interior Designer Job Application"
 
-puts "Please enter the following:"
+information_hash = {}
 
-information = {
-	:name =>
-	:age =>
-	:children =>
-	:decor =>
-}
+puts "Enter full name"
+full_name = gets.chomp
+
+information_hash["full_name"] = full_name
+
+puts "Enter age:"
+age = gets.chomp.to_i 
+
+information_hash["age"] = age
+ 
+puts "Enter number of children:"
+number_of_children = gets.chomp.to_i 
+
+information_hash["number_of_children"] = number_of_children
+ 
+puts "Enter decor theme"
+decor_theme = gets.chomp
+
+information_hash["decor_theme"] = decor_theme
+
+p information_hash
+#puts "Is this correct?"
+#information_hash.each do |key, value|
+ # puts "#{key} => #{value}"
+#end 
+ 
+# p information_hash
+puts "Do you want to edit your information?"
+edit_info = gets.chomp
+
+if edit_info == "yes"
+  # prompt them for key that they want to change
+  # promt them to enter correct name 
+  # overwrite the existing key value pair 
+end 
+
+
