2f06d7217ab38c15e328d7aff8050853924d59f4
Elizabeth Martinez <elizabethelzymartinez@gmail.com>
2017-02-27 15:15:57 -0600

    Add comment coding to 6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+
+=begin
+Species (Class)
+-canis lupus familiaris 
+
+characteristics (attributes)
+-size
+-color
+-age
+-breed
+
+behavior (methods)
+-aggression level
+-playfulness level
+-activeness level 
+=end 
+
