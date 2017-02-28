0eae5f2f6fa796b9ad5796dda36720e3c1be42b9
Elizabeth Martinez <elizabethelzymartinez@gmail.com>
2017-02-28 17:55:28 -0600

    Add code for 6.2 PPS

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,64 @@
-class Puppy
+# class Puppy
+#  def initialize
+#     puts "initialize new puppy instance..."
+#  end
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+#  def fetch(toy)
+#     puts "I brought back the #{toy}!"
+#     toy
+#   end
 
+#   def speak(int)
+#     puts int * 5
+#   end
+
+#   def roll_over
+#     puts "*rolls over*"
+#   end
+
+#   def dog_years(integer)
+#     puts integer * 7
+#   end 
+
+#   def jump
+#     puts "Jump!"
+#   end
+# end
+
+
+# puppy = Puppy.new 
+
+# puppy.fetch("toy")
+# puppy.speak("Woof!")
+# puppy.roll_over
+# puppy.dog_years(2)
+# puppy.jump
+
+
+class Automobile
+    def initialize
+      puts "2017 cars"
+    end  
+    def brand
+      puts "Toyota"
+    end
+
+    def model
+      puts "Yaris iA"
+    end
 end
+
+my_auto = []
+
+50.times_each do 
+  auto = Automobile.new
+  my_auto << auto
+end
+
+my_auto.each {|brand|}
+#missing release 2 #3
+
+
+
+
 
